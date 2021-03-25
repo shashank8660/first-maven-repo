94d07e7a817ed1733a87ad7c0868f5eb55455bbe
shashank8660 <himank.shashank@gmail.com>
2021-03-25 10:01:06 +0530

    First Commit

---
+++ first-maven-app/src/main/java/com/lti/model/Employee.java
@@ -0,0 +1,22 @@
+package com.lti.model;
+
+public class Employee {
+	
+	int emp_id;
+	String empName;
+	public int getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+	
+
+}
---
+++ first-maven-app/src/main/java/com/lti/view/Empview.java
@@ -0,0 +1,21 @@
+package com.lti.view;
+
+import com.lti.model.Employee;
+
+public class Empview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee emp  = new Employee();
+		
+		emp.setEmp_id(1001);
+		emp.setEmpName("Raj");
+		
+		System.out.println("Id is :"+ emp.getEmp_id()+" name is: "+emp.getEmpName());
+		
+		
+
+	}
+
+}
